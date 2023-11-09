aa5c9701281f5cd43daea61b0ba05d445b1f7508
Panga Jayakrishna <pangajayakrishna@Pangas-MacBook-Air.local>
2023-11-09 12:38:02 +0530

    local to remote

---
+++ database/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const USERNAME = process.env.DB_USERNAME;
+// const PASSWORD = process.env.DB_PASSWORD; 
+
+const Connection = async () => {
+    const DB_URI = "mongodb+srv://pangajk3:TNIl9NBVhymCC9u7@cluster0.pl4tppa.mongodb.net/?retryWrites=true&w=majority";
+    try {
+        await mongoose.connect(DB_URI, { useNewUrlParser: true });
+        mongoose.set('strictQuery', false);
+        console.log('Database connected sucessfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message)
+    }
+}
+export default Connection;
